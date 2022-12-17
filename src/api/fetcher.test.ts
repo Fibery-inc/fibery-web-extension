0fc9d1ec23199a682ab4a913b387eec571b12557
Maxim Filimonov <tpaktopsp@gmail.com>
2022-12-17 18:22:36 +0800

    remove - needs more thought

--- src/api/fetcher.test.ts
+++
@@ -1,9 +0,0 @@
-// @vitest-environment happy-dom
-
-import { useMe } from "./fetcher";
-import { test } from "vitest";
-import { renderHook } from "@testing-library/react";
-
-test("#useMe", () => {
-  renderHook(() => useMe());
-});
